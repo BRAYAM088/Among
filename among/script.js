9884afbccc7e6374cf63824b95318f5f1bb79b52
BRAYAM088 <68857618+BRAYAM088@users.noreply.github.com>
2020-10-13 14:45:30 -0500

    Add files via upload

---
+++ among/script.js
@@ -0,0 +1,110 @@
+function rojo() {
+    var rojo = prompt("El rojo está arreglando los cables, ¿es un impostor?");
+    rojo = rojo.toLowerCase();
+    if (rojo == "si") {
+        alert("El rojo no era Él impostor. 2 impostores restantes");
+    } else {
+        alert("Nadie fue eyectado. 2 impostores restantes");
+    }
+}
+
+function azul() {
+    var azul = prompt("El azul está en O2, ¿es un impostor?");
+    azul = azul.toLowerCase();
+    if (azul == "si") {
+        alert("El azul no era Él impostor. 2 Impostores restantes");
+    } else {
+        alert("Nadie fue eyectado. 2 Impostores restantes");
+    }
+}
+
+function oscuro() {
+    var oscuro = prompt("El verde oscuro esta MUERTO, ¿es un impostor?");
+    oscuro = oscuro.toLowerCase();
+    if (oscuro == "si") {
+        alert("El verde oscuro no era Él impostor. 2 impostores restantes");
+    } else {
+        alert("Nadie fue eyectado. 1 Impostor restante ");
+    }
+}
+function rosa() {
+    var rosa = prompt("El rosa vio a un tripulante meterse en la alcantarilla, ¿es un impostor?");
+    rosa = rosa.toLowerCase();
+    if (rosa == "si") {
+        alert("El rosa no era Él impostor. 2 impostores restantes");
+    } else {
+        alert("Nadie fue eyectado. 2 Impostores restantes ");
+    }
+}
+function naranja() {
+    var naranja = prompt("Naranja está en el reactor, ¿es un impostor?");
+    naranja = naranja.toLowerCase();
+    if (naranja == "si") {
+        alert("El naranja era Él impostor. 1 impostor restante");
+    } else {
+        alert("Nadie fue eyectado. 2 Impostor restantes ");
+    }
+}
+function amarillo() {
+    var amarillo = prompt("El amarillo está en cafetería descargando datos, ¿es un impostor?");
+    amarillo = amarillo.toLowerCase();
+    if (amarillo== "si") {
+        alert("el amarillo no era Él impostor. 2 impostores restantes ");
+    } else {
+        alert("Nadie fue eyectado. 2 Impostore restantes");
+    }
+}
+function Negro() {
+    var negro = prompt("Negro está MUERTO");
+    negro = negro.toLowerCase();
+    if (negro == "si") {
+        alert("El negro no era Él impostor. 1 impostor restante");
+    } else {
+        alert("Nadie fue eyectado. 1 Impostor restante ");
+    }
+}
+function blanco() {
+    var blanco = prompt("blanco está en admin y vio que alguien cambio de sala rapido, ¿es un impostor?");
+    blanco = blanco.toLowerCase();
+    if (blanco == "si") {
+        alert("El blanco no era Él impostor. 1 impostor restante ");
+    } else {
+        alert("Nadie fue eyectado. 1 Impostor restante ");
+    }
+}
+function morado() {
+    var morado = prompt("El morado está en la ventilacion, ¿es un impostor?");
+    morado = morado.toLowerCase();
+    if (morado == "si") {
+        alert("El morado era Él impostor. 1 impostor restante");
+    } else {
+        alert("Nadie fue eyectado. 2 Impostores restantes ");
+    }
+}
+function cafe() {
+    var cafe = prompt("cafe está en comunicaciones, ¿es un impostor?");
+    cafe = cafe.toLowerCase();
+    if (cafe == "si") {
+        alert("El rosa no era Él impostor. 1 impostor restante");
+    } else {
+        alert("Nadie fue eyectado. 1 Impostor restante ");
+    }
+}
+function celeste() {
+    var celeste = prompt("celeste está MUERTO, ¿es un impostor?");
+    celeste = celeste.toLowerCase();
+    if (celeste == "si") {
+        alert("celeste no era Él impostor. 1 impostor restante");
+    } else {
+        alert("Nadie fue eyectado. 1 Impostor restante ");
+    }
+}
+function verdel() {
+    var verdel = prompt("El verde claro está en camaras, ¿es un impostor?");
+    verdel = verdel.toLowerCase();
+    if (verdel == "si") {
+        alert("El verde claro no era Él impostor. 1 impostor restante");
+    } else {
+        alert("Nadie fue eyectado. 1 Impostor restante ");
+    }
+}
